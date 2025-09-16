372b23d38a18a903d4236342ac10bb1099c46af8
praveenkvofficial <praveenkumarvaishnavofficial@gmail.com>
2025-09-16 10:15:11 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import useFetch from "./useFetch";
 import "./App.css";
 
@@ -11,37 +10,21 @@ function App() {
 
       {loading && <p>Loading data...</p>}
 
-      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      {error && <p>Error: {error}</p>}
 
       {data && (
         <ul className="products">
           {data.map((item) => (
-            <ProductItem key={item.id} item={item} />
+            <li key={item.id} className="product-item">
+              <img src={item.images[0]} alt={item.title} />
+              <div>
+                <h3>{item.title}</h3>
+              </div>
+            </li>
           ))}
         </ul>
       )}
     </div>
-  );
-}
-
-// separate small component for each product
-function ProductItem({ item }) {
-  const [imgError, setImgError] = useState(false);
-
-  return (
-    <li className="product-item">
-      {/* show image only if not errored */}
-      {!imgError && (
-        <img
-          src={item.images[0]}
-          alt={item.title}
-          onError={() => setImgError(true)} // if image fails, hide it
-        />
-      )}
-      <div>
-        <h3>{item.title}</h3>
-      </div>
-    </li>
   );
 }
 
